cdb17326b540faf95a8c753c2bac70d699d19279
Leonardo Fontes <lfontes@fca.uncu.edu.ar>
2025-11-19 08:10:46 -0300

    Abre informe de labor desde menu

--- php/evaluacion.php
+++ php/evaluacion.php
@@ -7,11 +7,24 @@ $sql = "SELECT count(*) as cant_doc from reloj.agentes
         and legajo = $legajo ";
 $cantidad = toba::db('comision')->consultar_fila($sql);
 
-if ($cantidad['cant_doc'] > 0){
-$url = 'https://idd.fca.uncu.edu.ar/pruebas/';
-echo '<a href="' . $url . '" target="_blank">Sistema de Informe de Labor Docente</a>';
+// if ($cantidad['cant_doc'] > 0){
+// $url = 'https://idd.fca.uncu.edu.ar/pruebas/';
+// echo '<a href="' . $url . '" target="_blank">Sistema de Informe de Labor Docente</a>';
+// } else {
+//     echo 'Ud no es personal DOCENTE';
+// }
+// 
+
+// Abre la URL en una nueva pestaña si es docente
+if ($cantidad['cant_doc'] > 0) {
+    $url = 'http://localhost:7008/pruebas/1.0/';
+    echo "<script>window.open('$url', '_blank');</script>";
+    // Opcional: redirigir a otra página o cerrar la actual
+    // echo "<script>window.location.href = 'otra_pagina.php';</script>";
+    exit;
 } else {
     echo 'Ud no es personal DOCENTE';
 }
-?>
 
+
+?>
